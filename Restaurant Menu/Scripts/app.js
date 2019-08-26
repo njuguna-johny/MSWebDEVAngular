1f64d867790b34af21e5071676e10bce1e65190c
John Njuguna <njuguna.johny@gmail.com>
2019-08-26 14:13:13 +0300

    first commit

---
+++ Restaurant Menu/Scripts/app.js
@@ -0,0 +1,13 @@
+﻿var app = angular.module('restaurantApp', []);
+app.controller('menuCtrl', function ($scope, $http) {
+    $scope.menu = null;
+    $http.get('Data/menuData.json').then(successCallback, errorCallback);
+    function successCallback(response) {
+        $scope.menu = response.data;
+    }
+    function errorCallback(error) {
+        console.log(error);
+    }
+
+    
+});
